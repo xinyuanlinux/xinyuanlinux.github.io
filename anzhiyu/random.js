4faf38a22ef2aa615c26d1fc1cdfb2575c80fce9
xinyuan <2440229498@qq.com>
2024-07-20 15:21:04 +0800

    Site updated: 2024-07-20 15:21:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/19/hello-world/"];function toRandomPost(){
+var posts=["2024/07/20/嗨害嗨/","2024/07/19/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
