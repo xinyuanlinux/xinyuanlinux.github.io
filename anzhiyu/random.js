25b09a431d8f44bebcf848327595e6541566596e
520ZXY <2440229498@qq.com>
2024-12-09 09:29:27 +0800

    Site updated: 2024-12-09 09:29:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/21/山海随想/","2024/07/19/hello-world/","2024/07/28/smb/"];function toRandomPost(){
+var posts=["2024/07/19/hello-world/","2024/07/21/山海随想/","2024/07/28/smb/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
