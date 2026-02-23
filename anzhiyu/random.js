510ba299dcc8724145a983241c20a31564833a22
520ZXY <2440229498@qq.com>
2026-02-23 14:53:48 +0800

    Site updated: 2026-02-23 14:53:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/13/6DHCP中继实验/","2024/07/19/hello-world/","2024/07/28/smb/","2024/07/21/山海随想/"];function toRandomPost(){
+var posts=["2025/01/13/6DHCP中继实验/","2024/07/28/smb/","2024/07/19/hello-world/","2024/07/21/山海随想/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
